77f86946e7669cce2a6108d5100c5dc428859623
João Mateus Silva <jms.lopes@discente.ufma.br>
2024-09-08 22:52:35 -0300

    feat: Adicionar ProductService para gerenciar produtos

---
+++ src/main/java/com/curso/aula/services/ProductService.java
@@ -0,0 +1,53 @@
+package com.curso.aula.services;
+
+
+import com.curso.aula.dto.ProductDTO;
+import com.curso.aula.entities.Product;
+import com.curso.aula.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+
+
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id) {
+
+        Product product = repository.findById(id).get();
+         return new ProductDTO(product);
+
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findByAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
+    }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+        Product entity = new Product();
+        entity.setName(dto.getName());
+        entity.setPrice(dto.getPrice());
+        entity.setDescription(dto.getDescription());
+        entity.setImgUrl(dto.getImgUrl());
+
+        entity = repository.save(entity);
+
+        return new ProductDTO(entity);
+    }
+
+
+
+}
