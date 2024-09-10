7e727955f3de3ffcb47a0941ce7734a339ce16b0
João Mateus Silva <jms.lopes@discente.ufma.br>
2024-09-09 23:05:56 -0300

    Refactor ControllerExceptionHandler to handle DatabaseException

--- src/main/java/com/curso/aula/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/curso/aula/controllers/handlers/ControllerExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.curso.aula.controllers.handlers;
 
 import com.curso.aula.dto.CustomError;
+import com.curso.aula.services.exceptions.DatabaseException;
 import com.curso.aula.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,6 +18,13 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError error = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError error = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(error);
     }
